23898f8183bc4fb55bae50d45b7fac11fd17e677
Norman Lynch <lynchnf@gmail.com>
2018-05-16 15:11:16 -0500

    Make it a deployable war file.

--- src/main/java/norman/junk/App.java
+++ src/main/java/norman/junk/App.java
@@ -2,10 +2,17 @@ package norman.junk;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class App {
+public class App extends SpringBootServletInitializer {
     public static void main(String[] args) {
         SpringApplication.run(App.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder app) {
+        return app.sources(App.class);
     }
 }
